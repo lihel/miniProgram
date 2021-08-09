3fab9b7ad20d10a8d68a1021642018ca7feed88d
lihel <835943102@qq.com>
2021-08-09 18:00:43 +0800

    feat: 云函数

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,8 +2,8 @@
 	<view class="container">
 		<!-- <page-head></page-head> -->
 		<view class="intro">23333</view>
-		<text class="intro">详见：</text>
-		<uni-link :href="href" :text="href"></uni-link>
+		<!-- <text class="intro">详见：</text> -->
+		<!-- <uni-link :href="href" :text="href"></uni-link> -->
 	</view>
 </template>
 
@@ -14,6 +14,25 @@
 				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
 			}
 		},
+		created() {
+			uniCloud.callFunction({
+				//调用云函数
+				name: "signup",
+				//传的参数
+				data: {
+					"name": "xihongshi"
+				},
+				//成功回调
+				success(res) {
+					console.log('云函数调用成功', res.result);
+				},
+				//失败回调
+				fail(res) {
+					console.log('云函数调用失败', res);
+				}
+			})
+		},
+
 		methods: {
 
 		}
---
+++ uniCloud-aliyun/cloudfunctions/signup/index.js
@@ -0,0 +1,11 @@
+'use strict';
+exports.main = async (event, context) => {
+	//event为客户端上传的参数
+	console.log('event : ', event)
+	console.log('用户信息认证')
+	//返回数据给客户端
+	return {
+		"connect": 'success',
+		"event": event
+	}
+};
