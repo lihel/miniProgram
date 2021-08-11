bb1695a5caf1bf5ceaad513cf94a83e71802324d
lihel <835943102@qq.com>
2021-08-11 11:24:08 +0800

    f

--- uniCloud-aliyun/cloudfunctions/signup/index.js
+++ uniCloud-aliyun/cloudfunctions/signup/index.js
@@ -1,11 +1,12 @@
 'use strict';
+const db = uniCloud.database();
+
 exports.main = async (event, context) => {
 	//event为客户端上传的参数
-	console.log('event : ', event)
-	console.log('用户信息认证')
+	console.log(event)
+	const collection = db.collection('user-info')
+	const res = await collection.limit(10).get() // 获取表中的10条数据，结果为json格式
+	console.log(res)
 	//返回数据给客户端
-	return {
-		"connect": 'success',
-		"event": event
-	}
+	return res
 };
